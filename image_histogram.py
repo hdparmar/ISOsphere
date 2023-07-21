b91b1833539587d6af9f32a3cac4f58da1ad5362
Harshdeep Parmar <71045029+hdparmar@users.noreply.github.com>
2023-07-21 23:55:37 +0200

    colour histogram done

--- image_histogram.py
+++ image_histogram.py
@@ -3,7 +3,7 @@ import os
 import matplotlib.pyplot as plt
 
 # Path to the directory with the images
-directory_path = 'path_to_your_folder'
+directory_path = '../Film_01_CLC'
 
 # Get a list of all the file names in the directory
 image_files = os.listdir(directory_path)
@@ -13,22 +13,22 @@ for image_file in image_files:
     image_path = os.path.join(directory_path, image_file)
 
     # Read the image
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Read image as grayscale
+    image = cv2.imread(image_path)  # Read image as color
 
     # Check if image is not None (this means imread successfully read the image)
     if image is not None:
         # Display the image
         cv2.imshow('Image', image)
 
-        # Compute histogram
-        hist = cv2.calcHist([image], [0], None, [256], [0, 256])
-
-        # Plot histogram
+        # Compute histogram for each color channel and plot on same figure
+        colors = ('b', 'g', 'r')
         plt.figure()
-        plt.title("Grayscale Histogram")
+        plt.title('Color Histogram')
         plt.xlabel("Bins")
         plt.ylabel("# of Pixels")
-        plt.plot(hist)
+        for i, color in enumerate(colors):
+            hist = cv2.calcHist([image], [i], None, [256], [0, 256])
+            plt.plot(hist, color = color)
         plt.xlim([0, 256])
         plt.show()
 
