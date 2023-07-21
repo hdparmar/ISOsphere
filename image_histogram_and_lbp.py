5b69ee16e1d2b000eca713a14720e5bf0887a0d1
Harshdeep Parmar <71045029+hdparmar@users.noreply.github.com>
2023-07-21 23:59:44 +0200

    update

---
+++ image_histogram_and_lbp.py
@@ -0,0 +1,61 @@
+
+import cv2
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import feature
+
+# Path to the directory with the images
+directory_path = '../Film_01_CLC'
+
+# Get a list of all the file names in the directory
+image_files = os.listdir(directory_path)
+
+for image_file in image_files:
+    # Full path to the image file
+    image_path = os.path.join(directory_path, image_file)
+
+    # Read the image
+    image = cv2.imread(image_path)  # Read image as color
+
+    # Check if image is not None (this means imread successfully read the image)
+    if image is not None:
+        # Display the image
+        cv2.imshow('Image', image)
+
+        # Compute histogram for each color channel and plot on same figure
+        colors = ('b', 'g', 'r')
+        plt.figure()
+        plt.title('Color Histogram')
+        plt.xlabel("Bins")
+        plt.ylabel("# of Pixels")
+        for i, color in enumerate(colors):
+            hist = cv2.calcHist([image], [i], None, [256], [0, 256])
+            plt.plot(hist, color = color)
+        plt.xlim([0, 256])
+        plt.show()
+
+        # Convert image to grayscale for texture analysis
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        # Compute LBP
+        radius = 3
+        n_points = 8 * radius
+        lbp = feature.local_binary_pattern(gray, n_points, radius, method="uniform")
+
+        # Compute histogram of LBP
+        n_bins = int(lbp.max() + 1)
+        hist, _ = np.histogram(lbp, density=True, bins=n_bins, range=(0, n_bins))
+
+        # Plot histogram
+        plt.figure()
+        plt.title('LBP Histogram')
+        plt.xlabel("Bins")
+        plt.ylabel("# of Pixels")
+        plt.hist(hist, bins=n_bins)
+        plt.show()
+
+        # Wait for a key press and then close the image window
+        cv2.waitKey(0)
+
+cv2.destroyAllWindows()
